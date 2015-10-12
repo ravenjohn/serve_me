f2889ebdf4a7848302c39f52ba57d0ee108acfb0
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-10-12 15:34:30 +0800

    Initial commit

---
+++ serve_me.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+'use strict';
+
+const express = require('express');
+const app = express();
+
+if (!process.argv[2]) {
+	return console.log('path is missing');
+}
+
+
+app.use(express.static(process.cwd() + '/' + process.argv[2]));
+app.listen(process.argv[3] || 80);
+
+console.log('Serving', process.cwd() + '/' + process.argv[2], 'in port', process.argv[3] || 80);
